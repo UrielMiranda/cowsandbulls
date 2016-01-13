da78059afa440da5d2eeada8b832f5ae3b4a40e2
Uriel Miranda <urielnovoa@gmail.com>
2016-01-13 16:49:42 -0600

    add instructions

--- cowsandbulls.py
+++ cowsandbulls.py
@@ -1,27 +1,36 @@
-# Este es el juego de vacas y toros
-# Aleatoriamente "generar" un número de 4 dígitos. "Pida al usuario" que adivinar un número de 4 dígitos. "Por cada dígito que el usuario adivinó" correctamente
-# en el lugar correcto, tienen una "vaca". Por cada dígito del usuario adivinó correctamente en el lugar equivocado es un "toro". Cada vez que
-# el usuario realiza una conjetura, decirles cuántos "vacas" y "toros" tiene.
-# Una vez que el usuario adivina el número correcto, el juego ha terminado
-# Mantenga un "registro del número de intentos" que el usuario realiza en todo el juego y "mostrarlo al usuario" una vez finalizado el juego.
-
 import random
 
 
 def generate_digit():
         cows = 0
         bulls = 0
-        digit = str(random.randint(1000,9999))
+        numRandom = str(random.randint(1000, 9999))
+        print("\nRandom: ", numRandom)
         numUser = input("Numero de 4 digitos\n")
-        for i in digit:
-            for j in numUser:
-                if(j == i):
+        print("\nUser: ", numRandom)
+        print("Random: ", numUser)
+        for digit in numRandom:
+            print("Este es el valor de digit", digit)
+            for num in numUser:
+                if(num == digit):
                     bulls += 1
+            print("Este es el valor de num", numUser)
 
-        print("\nRandom: ",digit)
-        print("User: ",numUser)
-        print("\nTienes ",cows," cows y " ,bulls, " bulls")
+        # print("\nRandom: ",numRandom)
+        # print("User: ",numUser)
+        print("\nTienes ", cows, " cows y ", bulls, " bulls")
 
 
+# def check(index1,value1,index2,value2):
+#     if(value1 == value2):
+#         if(index1 == index2):
+#             return cow += 1
 
-generate_digit()
+
+def travel(elem, element):
+
+    for ele in element
+    pass
+
+
+# generate_digit()
